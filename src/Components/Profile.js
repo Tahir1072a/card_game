14524172c71d5f343e7200d96fb496cbbf834cfe
Tahir1072a <65045497+Tahir1072a@users.noreply.github.com>
2023-12-16 17:59:37 +0300

    Profile İskeleti oluşturuldu.

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,38 +1,58 @@
-function Gamer() {
+import { Gamer } from "./InMemory";
+import "../Css/kart.css";
+import { useState } from "react";
+
+const gamer = new Gamer(
+  0,
+  "Tahiri",
+  ["Yumruk"],
+  19,
+  19,
+  1,
+  process.env.PUBLIC_URL + `game/avatar1.jpeg`,
+);
+
+function GamerProfile() {
   return (
     <div className={"gamer"}>
       <img
-        src={"game/bomb.png"}
+        src={gamer.pngUrl}
         alt={"gamer_profile"}
         style={{ width: "10rem" }}
       />
-      <h2>Name</h2>
-      <HealthBar />
-      <div>Money</div>
+      <h2>{gamer.name}</h2>
+      <HealthBar health={gamer.health} shield={gamer.shield} />
+      <div>{gamer.money}</div>
       <select>
-        <option>Equipment1</option>
-        <option>Equipment2</option>
-        <option>Equipment3</option>
+        {gamer.equipments.map((p, i) => (
+          <option key={i} value={p}>
+            {p}
+          </option>
+        ))}
       </select>
     </div>
   );
 }
 
-function HealthBar() {
+function HealthBar({ health, shield }) {
+  const [hit, setHit] = useState(0);
+
   return (
     <>
       <div className={"healthBar"}>
         <div className={"bar"}>
-          <div className={"hit"}></div>
+          <div className={"hit"} style={{ width: `${hit - shield}rem` }}></div>
         </div>
       </div>
+      {/*burası shieldBar için */}
+
       <div className={"shieldBar"}>
         <div className={"bar"}>
-          <div className={"hit"}></div>
+          <div className={"hit"} style={{ width: `${hit}rem` }}></div>
         </div>
       </div>
     </>
   );
 }
 
-export default Gamer;
+export default GamerProfile;
