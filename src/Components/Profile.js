577f09607619f3a8380ec422e902725004620e42
Gökay Sağlam <123646071+bengokaysaglam@users.noreply.github.com>
2023-12-17 10:00:49 +0300

    profile kartı temel tasarımı yapıldı.

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -5,31 +5,44 @@ import { useState } from "react";
 const gamer = new Gamer(
   0,
   "Tahiri",
-  ["Yumruk"],
+  ["Yumruk", "Kılıç"],
   19,
   19,
-  1,
-  process.env.PUBLIC_URL + `game/avatar1.jpeg`,
+  10,
+  process.env.PUBLIC_URL + `game/avatar1.jpeg`
 );
 
 function GamerProfile() {
   return (
     <div className={"gamer"}>
-      <img
-        src={gamer.pngUrl}
-        alt={"gamer_profile"}
-        style={{ width: "10rem" }}
-      />
-      <h2>{gamer.name}</h2>
-      <HealthBar health={gamer.health} shield={gamer.shield} />
-      <div>{gamer.money}</div>
-      <select>
-        {gamer.equipments.map((p, i) => (
-          <option key={i} value={p}>
-            {p}
-          </option>
-        ))}
-      </select>
+      <div className="top">
+        <img
+          src={gamer.pngUrl}
+          alt={"gamer_profile"}
+          style={{ width: "7rem", borderRadius: "50%" }}
+        />
+
+        <div>
+          <h2 className="gamer-name">{gamer.name}</h2>
+          <HealthBar health={gamer.health} shield={gamer.shield} />
+        </div>
+      </div>
+
+      <div className="moves">
+        <div className="equipment">
+          <div>Equipment</div>
+
+          <select className="select">
+            {gamer.equipments.map((p, i) => (
+              <option key={i} value={p}>
+                {p}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="gamer-money">Balance: ${gamer.money}</div>
+      </div>
     </div>
   );
 }
@@ -38,20 +51,21 @@ function HealthBar({ health, shield }) {
   const [hit, setHit] = useState(0);
 
   return (
-    <>
-      <div className={"healthBar"}>
-        <div className={"bar"}>
+    <div className="stats">
+      <div className={"Bar"} style={{ width: "17rem" }}>
+        <div className={"healthBar"}>
           <div className={"hit"} style={{ width: `${hit - shield}rem` }}></div>
         </div>
       </div>
+
       {/*burası shieldBar için */}
 
-      <div className={"shieldBar"}>
-        <div className={"bar"}>
+      <div className={"Bar"}>
+        <div className={"shieldBar"}>
           <div className={"hit"} style={{ width: `${hit}rem` }}></div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
